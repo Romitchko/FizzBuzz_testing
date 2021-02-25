5ab70d226c7245da9dc225e69868d6ecf1c5a9c9
Romitchko <56961864+Romitchko@users.noreply.github.com>
2021-02-25 16:59:06 +0100

    commit 2
    commit buzz et fizz test2

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,8 +1,18 @@
 import unittest
+from unittest.case import TestCase
 
 from fizzbuzz_romain import Fizz, Buzz, FizzBuzz
 
 class fizzbuzz_programme (unittest.TestCase):
+
+    def fonction_fizz ():
+        
+        def erreur_si_input_vide ():
+            with unittest(TestCase) as exception_info:
+                Fizz()
+                assert exception_info.type == TestCase
+                assert "1 argument requis manquant" in str(exception_info.value)
+
     def retourne_Fizz_si_multiple_3 ():
         # est un multiple de 3 ?
         assert Fizz(3) == 'Fizz'        # multiple de 3
@@ -11,6 +21,16 @@ class fizzbuzz_programme (unittest.TestCase):
         assert Fizz(-3) == 'Fizz'       # multiple negatif de 3
         assert Fizz(-4) == -4           # negatif non-multiple de 3
         assert Fizz('Buzz') == 'Buzz'   # test input de string   
+
+#----------------------------
+    def fonction_buzz ():
+        
+        def erreur_si_input_vide ():
+            with unittest(TestCase) as exception_info:
+                Buzz()
+                assert exception_info.type == TestCase
+                assert "1 argument requis manquant" in str(exception_info.value)
+    
 
     def retourne_Buzz_si_multiple_5 ():
         # est un multiple de 3 ?
