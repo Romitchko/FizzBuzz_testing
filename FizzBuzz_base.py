0049f66340f836d900b78bd958835acc1e8b3437
Romitchko <56961864+Romitchko@users.noreply.github.com>
2021-02-25 17:47:24 +0100

    Update FizzBuzz_base.py

--- FizzBuzz_base.py
+++ FizzBuzz_base.py
@@ -1,15 +1,17 @@
 from fizzbuzz_romain import Fizz, Buzz, FizzBuzz
 
-x = int(input("Entrer un chiffre de 1 à 100"))
+nombreAlice = "Le nombre de Alice : "
+
+x = int(input("Bob : entre un chiffre de 1 à 100"))
 if x < 1:
     print("erreur")
 elif(x % 3 == 0 and x % 5 != 0):
     print("fizz")
 elif(x % 5 == 0 and x % 3 != 0):
     print("buzz")
-elif(x % 15 == 0):
+elif(x % 3 == 0 and x % 5 == 0):
     print("fizzbuzz")
 else:
-    print(x)
+    print("{}{}".format(nombreAlice, x))
 
     
