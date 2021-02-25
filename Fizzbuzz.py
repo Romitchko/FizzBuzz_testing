10b7f51b3966569e8639e433b977f1b57680c291
Romitchko <56961864+Romitchko@users.noreply.github.com>
2021-02-25 16:52:00 +0100

    Update Fizzbuzz.py

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -3,11 +3,20 @@ import unittest
 from fizzbuzz_romain import Fizz, Buzz, FizzBuzz
 
 class fizzbuzz_programme (unittest.TestCase):
-    def returns_Fizz_if_x_is_multiple_of_3 ():
+    def retourne_Fizz_si_multiple_3 ():
         # est un multiple de 3 ?
         assert Fizz(3) == 'Fizz'        # multiple de 3
         assert Fizz(2) == 2             # non multiple de 3
         assert Fizz(0) == 'Fizz'        # rien
         assert Fizz(-3) == 'Fizz'       # multiple negatif de 3
         assert Fizz(-4) == -4           # negatif non-multiple de 3
-        assert Fizz('Buzz') == 'Buzz'   # test input de string      
+        assert Fizz('Buzz') == 'Buzz'   # test input de string   
+
+    def retourne_Buzz_si_multiple_5 ():
+        # est un multiple de 3 ?
+        assert Buzz(5) == 'Buzz'        # multiple de 5
+        assert Buzz(2) == 2             # non multiple de 5
+        assert Buzz(0) == 'Buzz'        # rien
+        assert Buzz(-5) == 'Buzz'       # multiple negatif de 5
+        assert Buzz(-4) == -4           # negatif non-multiple de 5
+        assert Buzz('Fizz') == 'Fizz'   # test input de string    
